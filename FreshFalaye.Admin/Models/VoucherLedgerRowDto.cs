b0cddb812f5e1a358f3fa16da067af3d831a0266
Sandeep <joshi.25387@gmail.com>
2026-02-05 13:33:39 +0530

    Updated Vouchers Grid screen

---
+++ FreshFalaye.Admin/Models/VoucherLedgerRowDto.cs
@@ -0,0 +1,21 @@
+﻿namespace FreshFalaye.Admin.Models
+{
+    public class VoucherLedgerRowDto
+    {
+        // MASTER
+        public Guid Id { get; set; }
+        public DateTime VoucherDate { get; set; }
+        public string VoucherNo { get; set; }
+        public string MainAccountName { get; set; }
+
+        // DETAIL
+        public string CreditAccountName { get; set; }
+        public string DebitAccountName { get; set; }
+        public decimal? Amount { get; set; }        
+        public string RefNo { get; set; }
+        public string Remarks { get; set; }
+        public string Narration { get; set; }
+
+    }
+
+}
